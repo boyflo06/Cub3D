76cdc2490a804a025394520e55e268d2afa7896f
MaximWDK <maxim.leonet@gmail.com>
2024-11-19 19:02:19 +0100

    Check map is valid

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: mleonet <mleonet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:52:34 by mleonet           #+#    #+#             */
-/*   Updated: 2024/11/19 16:50:52 by mleonet          ###   ########.fr       */
+/*   Updated: 2024/11/19 19:01:51 by mleonet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,49 @@ int	openimages(t_prog *prog)
 	return (1);
 }
 
+int	check_map_content(t_prog *prog)
+{
+	int	i;
+	int	j;
+	int	player;
+
+	i = -1;
+	player = 0;
+	while (prog->map.data[++i])
+	{
+		j = -1;
+		while (prog->map.data[i][++j])
+		{
+			if (!ft_strchr(" 01NSWE", prog->map.data[i][j]))
+				return (0);
+			if (ft_strchr("NSWE", prog->map.data[i][j]))
+				player++;
+			if (i == 0 || i == prog->map.height - 1 || j == 0 || j == prog->map.width - 1)
+			{
+				if (!ft_strchr(" 1", prog->map.data[i][j]))
+					return (0);
+			}
+			if (prog->map.data[i][j] == ' ' && i < prog->map.height
+				&& j < prog->map.width - 1)
+			{
+				if (i > 0 && !ft_strchr(" 1", prog->map.data[i - 1][j]))
+					return (0);
+				if (i < prog->map.height - 1
+					&& !ft_strchr(" 1", prog->map.data[i + 1][j]))
+					return (0);
+				if (j > 0 && !ft_strchr(" 1", prog->map.data[i][j - 1]))
+					return (0);
+				if (j < prog->map.width - 1
+					&& !ft_strchr(" 1", prog->map.data[i][j + 1]))
+					return (0);
+			}
+		}
+		if (player > 1)
+			return (0);
+	}
+	return (1);
+}
+
 int	parsemap(t_prog *prog)
 {
 	int		i;
@@ -85,6 +128,8 @@ int	parsemap(t_prog *prog)
 	prog->map.width = width;
 	if (!add_spaces_map(prog))
 		return (write(2, "Error\nCub3D: Malloc failed\n", 27) - 27);
+	if (!check_map_content(prog))
+		return (write(2, "Error\nCub3D: Invalid map\n", 25) - 25);
 	return (1);
 }
 
